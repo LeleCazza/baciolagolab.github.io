c906b28792b54083c364e84b556e59ddd58442ac
Gabriele Cazzaniga <leye.lele@gmail.com>
2021-10-21 17:22:45 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it-IT">
+
+<head>
+	<link rel="stylesheet" href="css/styles.css">
+	<title>BACIOLAGOLAB</title>
+</head>
+
+<body>
+	<img class="home" src="/immagini/home.png">
+	<div class="principale">
+		<p>Benvenuti al BACIOLAGOLAB</p>
+	</div>
+</body>
+
+</html>
